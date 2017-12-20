462eb54369e742cee1e06db9299cdac07387790d
Muhammad Furqan Habibi <furqan.habibi1@gmail.com>
2017-12-20 20:43:12 +0900

    finish image viewer

---
+++ src/image_view_node.cpp
@@ -0,0 +1,36 @@
+// Copyright 2015 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "image_view_node.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <memory>
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  std::string topic_name;
+  if (argc >= 2) {
+    auto image_view_node = std::make_shared<ImageViewNode>(argv[1], "image_view_node", false);
+    rclcpp::spin(image_view_node);
+  }
+  else {
+    std::cout << "Input the image topic!\n";
+  }
+
+  rclcpp::shutdown();
+
+  return 0;
+}
